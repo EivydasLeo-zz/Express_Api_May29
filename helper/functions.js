8dce4a4071c4abcafb51fc8d30a73843a8cbd2ab
Your Name <you@example.com>
2021-05-29 21:42:23 +0300

    handleFindErr fn + delete todo start

--- helper/functions.js
+++ helper/functions.js
@@ -5,6 +5,12 @@ function findTodoWithId(id) {
   return todoDb.find((t) => t.id === +id);
 }
 
+function handleFindErr(paramId, res) {
+  console.log('todo not found');
+  return res.status(400).json({ error: `todo with id: ${paramId} does not exist` });
+}
+
 module.exports = {
   findTodoWithId,
+  handleFindErr,
 };
